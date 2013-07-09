bc6507f7d9bda30253185bd464cbc6480d8f43d0
Alvin Lazaro <me@alvinl.com>
2013-07-09 01:19:53 -0400

    change the way bot logs in

--- handler.js
+++ handler.js
@@ -23,12 +23,24 @@ exports.sentry = function(hash){
 // Error event
 exports.error = function(e){
   log.error(e);
+  if (e.eresult === 65) {
+    log.warn('Enter Steamcode below');
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.once('data', function(data) {
+      var code = data.replace('\n', '');
+      app.bot.logOn(config.username, config.password, code);
+    });
+  } else if (e.eresult === 5) {
+    log.error('Invalid password, please check that the password in your config is correct');
+  }
 };
 
 
 // Logged In event
 exports.loggedOn = function(){
   log.info('Bot Logged in!');
+
   app.bot.setPersonaState(app.steam.EPersonaState.Online);
 
   if (config.botname) {
